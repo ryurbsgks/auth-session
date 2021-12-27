2dc2b757996b9ebda82dfbd3fd05e73430f0bbe7
ryurbsgks <ryurbsgks5114@gmail.com>
2021-12-27 16:20:45 +0900

    Server Bare minimum requirement 완료

--- server-session/controller/users/login.js
+++ server-session/controller/users/login.js
@@ -11,11 +11,16 @@ module.exports = {
 
     // TODO: userInfo 결과 존재 여부에 따라 응답을 구현하세요.
     // 결과가 존재하는 경우 세션 객체에 userId가 저장되어야 합니다.
-    if (!result) {
+    if (!userInfo) {
       // your code here
+      res.status(400).send({data:null, message:"not authorized"});
     } else {
       // your code here
       // HINT: req.session을 사용하세요.
+      req.session.save(function(err){
+        req.session.userId=userInfo.userId;
+        res.status(200).send({data:userInfo, message:"ok"}) 
+      })
     }
   }
 }
--- server-session/controller/users/logout.js
+++ server-session/controller/users/logout.js
@@ -5,11 +5,15 @@ module.exports = {
     // 앞서 로그인시 세션 객체에 저장했던 값이 존재할 경우, 이미 로그인한 상태로 판단할 수 있습니다.
     // 세션 객체에 담긴 값의 존재 여부에 따라 응답을 구현하세요.
 
-    if (!req.session.FILL_ME_IN) {
+    if (!req.session.userId) {
       // your code here
+      res.status(400).send({data:null, messages:"not authorized"});
     } else {
       // your code here
       // TODO: 로그아웃 요청은 세션을 삭제하는 과정을 포함해야 합니다.
+      req.session.destroy(function(err){
+        res.status(200).send({data:null, message:"ok"});
+      })
     }
   },
 };
